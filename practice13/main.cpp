7ffbc9e5eda25e4fd474da0fc12440e8cd119611
nalek0 <belash.aleksandr777@gmail.com>
2025-03-30 17:16:28 +0300

    Task 5

--- practice13/main.cpp
+++ practice13/main.cpp
@@ -147,6 +147,14 @@ GLuint create_program(Shaders ... shaders)
     return result;
 }
 
+struct animation_translation {
+    std::string old_animation;
+    std::string new_animation;
+    float current_time;
+    float translation_time;
+    bool active;
+};
+
 int main() try
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -282,6 +290,10 @@ int main() try
     float camera_rotation = 0.f;
     float camera_height = 1.f;
 
+    std::string animation_name = "hip-hop";
+    animation_translation translation;
+    translation.active = false;
+
     bool paused = false;
 
     bool running = true;
@@ -336,6 +348,35 @@ int main() try
         if (button_down[SDLK_s])
             view_angle += 2.f * dt;
 
+
+        if (button_down[SDLK_1] && !translation.active) {
+            translation.old_animation = animation_name;
+            translation.new_animation = "hip-hop";
+            translation.current_time = time;
+            translation.translation_time = 0.5f;
+            translation.active = true;
+
+            animation_name = "hip-hop";
+        }
+        if (button_down[SDLK_2] && !translation.active) {
+            translation.old_animation = animation_name;
+            translation.new_animation = "rumba";
+            translation.current_time = time;
+            translation.translation_time = 0.5f;
+            translation.active = true;
+
+            animation_name = "rumba";
+        }
+        if (button_down[SDLK_3] && !translation.active) {
+            translation.old_animation = animation_name;
+            translation.new_animation = "flair";
+            translation.current_time = time;
+            translation.translation_time = 0.5f;
+            translation.active = true;
+
+            animation_name = "flair";
+        }
+
         glClearColor(0.8f, 0.8f, 1.f, 0.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -345,7 +386,6 @@ int main() try
 
         float near = 0.1f;
         float far = 100.f;
-        float scale = 0.75 + cos(time) * 0.25;
 
         glm::mat4 model = glm::scale(glm::mat4(1.f), glm::vec3(0.01f));
 
@@ -363,32 +403,78 @@ int main() try
 
         const int bones_amount = input_model.bones.size();
         std::vector<glm::mat4x3> bones(bones_amount);
+        if (translation.active) {
+            gltf_model::animation old_bones_animation = input_model.animations.at(translation.old_animation);
+            gltf_model::animation new_bones_animation = input_model.animations.at(translation.new_animation);
+            float old_frame_start = std::fmod(translation.current_time, old_bones_animation.max_time);
+            float new_frame_start = std::fmod(translation.current_time, new_bones_animation.max_time);
+            float delta_time = time - translation.current_time;
+            float max_time = translation.translation_time;
+            float old_frame = std::fmod(old_frame_start + delta_time, old_bones_animation.max_time);
+            float new_frame = std::fmod(new_frame_start + delta_time, new_bones_animation.max_time);
+            
+            if (delta_time > max_time) {
+                translation.active = false;
+            } else {
+                for (int i = 0; i < bones_amount; i++) {
+                    glm::mat4 transform;
+                    {
+                        gltf_model::bone_animation old_bone_animation = old_bones_animation.bones[i];
+                        gltf_model::bone_animation new_bone_animation = new_bones_animation.bones[i];
 
-        std::string animation_name = "hip-hop";
-        gltf_model::animation bones_animation = input_model.animations.at(animation_name);
-        std::vector<glm::mat4> bones_transforms;
-
-        for (int i = 0; i < bones_amount; i++) {
-            glm::mat4 transform;
-            {
-                float frame = std::fmod(time, bones_animation.max_time);
-                gltf_model::bone_animation bone_animation = bones_animation.bones[i];
-                glm::mat4 translation = glm::translate(glm::mat4(1.f), bone_animation.translation(frame));
-                glm::mat4 rotation = glm::toMat4(bone_animation.rotation(frame));
-                glm::mat4 scale = glm::scale(glm::mat4(1.f), bone_animation.scale(frame));
-                transform = translation * rotation * scale;
+                        auto old_translation = old_bone_animation.translation(old_frame);
+                        auto old_rotation = old_bone_animation.rotation(old_frame);
+                        auto old_scale = old_bone_animation.scale(old_frame);
+                        auto new_translation = new_bone_animation.translation(new_frame);
+                        auto new_rotation = new_bone_animation.rotation(new_frame);
+                        auto new_scale = new_bone_animation.scale(new_frame);
+    
+                        glm::mat4 translation = glm::translate(glm::mat4(1.f), glm::lerp(old_translation, new_translation, delta_time / max_time));
+                        glm::mat4 rotation = glm::toMat4(glm::slerp(old_rotation, new_rotation, delta_time / max_time));
+                        glm::mat4 scale = glm::scale(glm::mat4(1.f), glm::lerp(old_scale, new_scale, delta_time / max_time));
+    
+                        transform = translation * rotation * scale;
+                    }
+        
+                    if (input_model.bones[i].parent != -1) {
+                        glm::mat4x3 parent_transform = bones[input_model.bones[i].parent];
+                        transform = parent_transform * transform;
+                    }
+        
+                    bones[i] = transform;
+                }
+        
+                for (int i = 0; i < bones_amount; i++) {
+                    bones[i] = bones[i] * input_model.bones[i].inverse_bind_matrix;
+                }
             }
-
-            if (input_model.bones[i].parent != -1) {
-                glm::mat4x3 parent_transform = bones[input_model.bones[i].parent];
-                transform = parent_transform * transform;
-            }
-
-            bones[i] = transform;
         }
-
-        for (int i = 0; i < bones_amount; i++) {
-            bones[i] = bones[i] * input_model.bones[i].inverse_bind_matrix;
+        
+        if (!translation.active) {
+            gltf_model::animation bones_animation = input_model.animations.at(animation_name);
+    
+            for (int i = 0; i < bones_amount; i++) {
+                glm::mat4 transform;
+                {
+                    float frame = std::fmod(time, bones_animation.max_time);
+                    gltf_model::bone_animation bone_animation = bones_animation.bones[i];
+                    glm::mat4 translation = glm::translate(glm::mat4(1.f), bone_animation.translation(frame));
+                    glm::mat4 rotation = glm::toMat4(bone_animation.rotation(frame));
+                    glm::mat4 scale = glm::scale(glm::mat4(1.f), bone_animation.scale(frame));
+                    transform = translation * rotation * scale;
+                }
+    
+                if (input_model.bones[i].parent != -1) {
+                    glm::mat4x3 parent_transform = bones[input_model.bones[i].parent];
+                    transform = parent_transform * transform;
+                }
+    
+                bones[i] = transform;
+            }
+    
+            for (int i = 0; i < bones_amount; i++) {
+                bones[i] = bones[i] * input_model.bones[i].inverse_bind_matrix;
+            }
         }
 
         glUseProgram(program);
